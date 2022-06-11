1d9490bef33361c4d2eb04de844298c16a5ac532
Gustavo Barbosa <88351700+ogustavobarbosa@users.noreply.github.com>
2022-06-11 00:45:31 -0300

    Update banco.php

--- banco.php
+++ banco.php
@@ -3,27 +3,19 @@
 require 'src/Conta.php';
 require 'src/Titular.php';
 
-
 $gustavo  = new Titular('Gustavo', '123.324.455-23');
 $marcos  = new Titular('Marcos', '345.345.455-23');
-
-
 
 $conta1 = new Conta($marcos);
 $conta2 = new Conta($gustavo);
 
 $conta1->depositar(1200);
-
-
 $conta1->transferir(700, $conta2);
 
 
 echo $conta1->recuperaSaldo(). PHP_EOL;
 
-
 echo $conta2->recuperaSaldo(). PHP_EOL;
-
-
 
 echo Conta::recuperaNumeroDeContas(). PHP_EOL;
 
